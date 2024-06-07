ffe98e26b08c648e056428d095ff2276fdbf61a8
EmilijaNikolic333 <nikolicemilija03@gmail.com>
2024-06-07 17:41:12 +0200

    funkcionalnosti na formi

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,24 +4,35 @@ import './Login.css'; // Importovanje CSS fajla
 const Login: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     const handleLogin = () => {
-        // Ovde možete dodati logiku za proveru korisničkih podataka
-        console.log('Korisničko ime:', username);
-        console.log('Lozinka:', password);
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (username.length <= 8) {
+            setError('Korisničko ime mora biti duže od osam karaktera.');
+        } else if (!password) {
+            setError('Molimo unesite lozinku.');
+        } else if (!passwordPattern.test(password)) {
+            setError('Lozinka mora sadržati mala i velika slova, broj i specijalni znak.');
+        } else {
+            // Ovde možete dodati logiku za proveru korisničkih podataka
+            console.log('Korisničko ime:', username);
+            console.log('Lozinka:', password);
+        }
     };
 
     return (
-        <div className = "container">
-            
+        <div className="container"> {/* Dodavanje CSS klase na div */}
+            <h1>Login Page</h1>
             <div className="form-group">
                 <label htmlFor="username">Korisničko ime:</label>
                 <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
-            <div>
+            <div className="form-group">
                 <label htmlFor="password">Lozinka:</label>
                 <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
+            {error && <p className="error">{error}</p>} {/* Prikazivanje poruke o grešci ako postoji */}
             <div className="button-container"> {/* Dodajemo dodatni div oko dugmeta */}
                 <button onClick={handleLogin}>Prijavi se</button>
             </div>
